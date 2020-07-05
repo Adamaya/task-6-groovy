959debb3ba6a6218dae50ccee9bade8b6179f106
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-07-05 14:37:01 +0530

    Update configure.groovy

--- configure.groovy
+++ configure.groovy
@@ -1,4 +1,4 @@
-job('example-1') {
+job('job1_pull_repo_build_image') {
     steps {
       scm {
         git {
@@ -60,4 +60,43 @@ job('example-1') {
             }
         }
     }
+}
+
+job('job2_deploy_on_k8s'){
+  steps{
+    triggers{
+	  upstream("job1_pull_repo_build_image","SUCCESS")
+    }
+    shell("count=\$(ls /var/lib/jenkins/workspace/job1 | grep .php | wc -l);if [[ \$count -gt 0 ]];then if kubectl get deployment | grep php; then exit 0 ;else kubectl create -f /home/php-deployment.yml ;fi; else if kubectl get deployment | grep http; then exit 0; else kubectl create -f /home/http-deployment.yml; fi;fi")
+  }
+}
+
+job('job3_test_web_app'){
+  steps{
+    triggers{
+	  upstream("job2_deploy_on_k8s","SUCCESS")
+    }
+    shell('sleep 5;status=\$(curl -o /dev/null -s -w "%{http_code}" 192.168.99.102:30600); if [[ \$status == 200 ]]; then echo "web application working" ;else echo "web application is not working";fi')
+  }
+}
+
+job('job4_redeploy'){
+	triggers {
+    upstream {
+      upstreamProjects("job3_test_web_app")
+      threshold("FAILURE")
+    }
+  }
+  
+  
+  publishers {
+    postBuildScripts {
+      steps {
+        downstreamParameterized {
+  	  	  trigger("job1_pull_repo_build_image")
+        }
+      }
+    }
+  }
+}
 }
